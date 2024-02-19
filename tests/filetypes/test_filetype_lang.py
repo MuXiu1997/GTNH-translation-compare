0b77aecda21a5eb368220fbe6b63cf1d8cb32a5c
IcaroKaue <73764500+IcaroKaue@users.noreply.github.com>
2024-02-18 23:17:01 -0300

    Adicionado suporte a pr_BR.

--- tests/filetypes/test_filetype_lang.py
+++ tests/filetypes/test_filetype_lang.py
@@ -31,6 +31,16 @@ JA_JP_CONTENT = "\n".join(
         "test3",
     ]
 )
+PT_BR_RELPATH = "test/x/pt_BR.lang"
+PT_BR_CONTENT = "\n".join(
+    [
+        "#teste",
+        "test=teste",
+        "",
+        "test2=teste2=teste2",
+        "test3",
+    ]
+)
 
 
 @pytest.fixture(scope="module")
@@ -47,25 +57,31 @@ def zh_cn_filetype_lang() -> FiletypeLang:
 def ja_jp_filetype_lang() -> FiletypeLang:
     return FiletypeLang(JA_JP_RELPATH, JA_JP_CONTENT, Language.ja_JP)
 
+@pytest.fixture(scope="module")
+def pt_br_filetype_lang() -> FiletypeLang:
+    return FiletypeLang(PT_BR_RELPATH, PT_BR_CONTENT, Language.pt_BR)
+
 
 def test__get_relpath(
-    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang
+    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang, pt_br_filetype_lang: FiletypeLang
 ) -> None:
     assert en_us_filetype_lang.relpath == EN_US_RELPATH
     assert zh_cn_filetype_lang.relpath == ZH_CN_RELPATH
     assert ja_jp_filetype_lang.relpath == JA_JP_RELPATH
+    assert pt_br_filetype_lang.relpath == PT_BR_RELPATH
 
 
 def test__get_content(
-    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang
+    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang, pt_br_filetype_lang: FiletypeLang
 ) -> None:
     assert en_us_filetype_lang.content == EN_US_CONTENT
     assert zh_cn_filetype_lang.content == ZH_CN_CONTENT
     assert ja_jp_filetype_lang.content == JA_JP_CONTENT
+    assert pt_br_filetype_lang.relpath == PT_BR_RELPATH
 
 
 def test__get_properties(
-    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang
+    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang, pt_br_filetype_lang: FiletypeLang
 ) -> None:
     assert en_us_filetype_lang.properties == {
         "lang|test": Property("lang|test", "test", "test=test", 11, 15),
@@ -79,19 +95,26 @@ def test__get_properties(
         "lang|test": Property("lang|test", "テスト", "test=テスト", 11, 14),
         "lang|test2": Property("lang|test2", "テスト2=テスト2", "test2=テスト2=テスト2", 22, 31),
     }
+    assert pt_br_filetype_lang.properties == {
+        "lang|test": Property("lang|test", "teste", "test=teste", 11, 14),
+        "lang|test2": Property("lang|test2", "teste2=teste2", "test2=teste2=teste2", 22, 31),
+    }
 
 
 def test_get_en_us_relpath(
-    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang
+    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang, pt_br_filetype_lang: FiletypeLang
 ) -> None:
     assert en_us_filetype_lang.get_en_us_relpath() == EN_US_RELPATH
     assert zh_cn_filetype_lang.get_en_us_relpath() == EN_US_RELPATH
     assert ja_jp_filetype_lang.get_en_us_relpath() == EN_US_RELPATH
+    assert pt_br_filetype_lang.get_en_us_relpath() == EN_US_RELPATH
 
 
 def test_get_zh_cn_relpath(
-    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang
+    en_us_filetype_lang: FiletypeLang, zh_cn_filetype_lang: FiletypeLang, ja_jp_filetype_lang: FiletypeLang, pt_br_filetype_lang: FiletypeLang
 ) -> None:
-    assert en_us_filetype_lang.get_target_language_relpath(Language.zh_CN) == ZH_CN_RELPATH
-    assert zh_cn_filetype_lang.get_target_language_relpath(Language.ja_JP) == JA_JP_RELPATH
-    assert ja_jp_filetype_lang.get_target_language_relpath(Language.zh_CN) == ZH_CN_RELPATH
+    assert en_us_filetype_lang.get_target_language_relpath(Language.en_US) == EN_US_RELPATH
+    assert zh_cn_filetype_lang.get_target_language_relpath(Language.zh_CN) == ZH_CN_RELPATH
+    assert ja_jp_filetype_lang.get_target_language_relpath(Language.ja_JP) == JA_JP_RELPATH
+    assert pt_br_filetype_lang.get_target_language_relpath(Language.pt_BR) == PT_BR_RELPATH
+
