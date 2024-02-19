9173433aa333389a190903843866172c5065b94d
IcaroKaue <73764500+IcaroKaue@users.noreply.github.com>
2024-02-18 23:12:14 -0300

    Adicionado suporte a pt_BR.

--- tests/filetypes/test_filetype_gt_lang.py
+++ tests/filetypes/test_filetype_gt_lang.py
@@ -51,7 +51,23 @@ JA_JP_CONTENT = "\n".join(
         "",
     ]
 )
-
+PT_BR_RELPATH = "GregTech.lang"
+PT_BR_CONTENT = "\n".join(
+    [
+        "# Configuration file",
+        "",
+        "enablelangfile {",
+        "    S:Language=en_US",
+        "    B:UseThisFileAsLanguageFile=true",
+        "}",
+        "",
+        "",
+        "languagefile {",
+        "    S:test=teste",
+        "}",
+        "",
+    ]
+)
 
 @pytest.fixture(scope="module")
 def en_us_filetype_gt_lang() -> FiletypeGTLang:
@@ -67,31 +83,40 @@ def zh_cn_filetype_gt_lang() -> FiletypeGTLang:
 def ja_jp_filetype_gt_lang() -> FiletypeGTLang:
     return FiletypeGTLang(JA_JP_RELPATH, JA_JP_CONTENT, Language.ja_JP)
 
+@pytest.fixture(scope="module")
+def pt_br_filetype_gt_lang() -> FiletypeGTLang:
+    return FiletypeGTLang(PT_BR_RELPATH, PT_BR_CONTENT, Language.pt_BR)
+
 
 def test__get_relpath(
     en_us_filetype_gt_lang: FiletypeGTLang,
     zh_cn_filetype_gt_lang: FiletypeGTLang,
     ja_jp_filetype_gt_lang: FiletypeGTLang,
+    pt_br_filetype_gt_lang: FiletypeGTLang,
 ) -> None:
     assert en_us_filetype_gt_lang.relpath == EN_US_RELPATH
     assert zh_cn_filetype_gt_lang.relpath == ZH_CN_RELPATH
     assert ja_jp_filetype_gt_lang.relpath == JA_JP_RELPATH
+    assert pt_br_filetype_gt_lang.relpath == PT_BR_RELPATH
 
 
 def test__get_content(
     en_us_filetype_gt_lang: FiletypeGTLang,
     zh_cn_filetype_gt_lang: FiletypeGTLang,
     ja_jp_filetype_gt_lang: FiletypeGTLang,
+    pt_br_filetype_gt_lang: FiletypeGTLang,
 ) -> None:
     assert en_us_filetype_gt_lang.content == EN_US_CONTENT
     assert zh_cn_filetype_gt_lang.content == ZH_CN_CONTENT
     assert ja_jp_filetype_gt_lang.content == JA_JP_CONTENT
+    assert pt_br_filetype_gt_lang.content == PT_BR_CONTENT
 
 
 def test__get_properties(
     en_us_filetype_gt_lang: FiletypeGTLang,
     zh_cn_filetype_gt_lang: FiletypeGTLang,
     ja_jp_filetype_gt_lang: FiletypeGTLang,
+    pt_br_filetype_gt_lang: FiletypeGTLang,
 ) -> None:
     assert en_us_filetype_gt_lang.properties == {
         "gt-lang|    S:test": Property("gt-lang|    S:test", "test", "    S:test=test", 107, 111),
@@ -102,23 +127,31 @@ def test__get_properties(
     assert ja_jp_filetype_gt_lang.properties == {
         "gt-lang|    S:test": Property("gt-lang|    S:test", "テスト", "    S:test=テスト", 127, 130),
     }
+    assert pt_br_filetype_gt_lang.properties == {
+        "gt-lang|    S:test": Property("gt-lang|    S:test", "teste", "    S:test=teste", 127, 130),
+    }
+
 
 
 def test_get_en_us_relpath(
     en_us_filetype_gt_lang: FiletypeGTLang,
     zh_cn_filetype_gt_lang: FiletypeGTLang,
     ja_jp_filetype_gt_lang: FiletypeGTLang,
+    pt_br_filetype_gt_lang: FiletypeGTLang,
 ) -> None:
     assert en_us_filetype_gt_lang.get_en_us_relpath() == EN_US_RELPATH
     assert zh_cn_filetype_gt_lang.get_en_us_relpath() == EN_US_RELPATH
     assert ja_jp_filetype_gt_lang.get_en_us_relpath() == EN_US_RELPATH
+    assert pt_br_filetype_gt_lang.get_en_us_relpath() == EN_US_RELPATH
 
 
 def test_get_zh_cn_relpath(
     en_us_filetype_gt_lang: FiletypeGTLang,
     zh_cn_filetype_gt_lang: FiletypeGTLang,
     ja_jp_filetype_gt_lang: FiletypeGTLang,
+    pt_br_filetype_gt_lang: FiletypeGTLang,
 ) -> None:
-    assert en_us_filetype_gt_lang.get_target_language_relpath(Language.zh_CN) == ZH_CN_RELPATH
-    assert zh_cn_filetype_gt_lang.get_target_language_relpath(Language.ja_JP) == JA_JP_RELPATH
-    assert ja_jp_filetype_gt_lang.get_target_language_relpath(Language.zh_CN) == ZH_CN_RELPATH
+    assert en_us_filetype_gt_lang.get_target_language_relpath(Language.en_US) == EN_US_RELPATH
+    assert zh_cn_filetype_gt_lang.get_target_language_relpath(Language.zh_CN) == ZH_CN_RELPATH
+    assert ja_jp_filetype_gt_lang.get_target_language_relpath(Language.ja_JP) == JA_JP_RELPATH
+    assert pt_br_filetype_gt_lang.get_target_language_relpath(Language.pt_BR) == PT_BR_RELPATH
